32601491b461790e78777c79d69d779942dbd0f6
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-04 13:39:45 +1100

    Add project files.

---
+++ GenCommon.Shared/Extensions/JsonExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenCommon.Shared.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static string ToJson<T>(this T obj)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj);
+            }
+            catch (Exception e)
+            {
+                return default;
+            }
+        }
+
+        public static T ToObj<T>(this string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (Exception e)
+            {
+                return default;
+            }
+        }
+    }
+}
---
+++ GenCommon.Shared/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenCommon.Shared
+{
+    public static class Settings
+    {
+        public static readonly string SmtpHost = "smtp.zoho.com.au";
+        public static readonly int SmtpPort = 587;
+        public static readonly bool SmtpUseSsl = true;
+        public static readonly string SmtpSender = "malavdea@zohomail.com.au";
+        public static readonly string SmtpPassword = "wSxzMhNxJ1NU";
+        public static readonly int EmailValidationSize = 200;
+        public static readonly int PhoneValidationSize = 6;
+        public static readonly string SmsSender = "malavdea";
+        public static readonly string ClickSendUsername = "atalmalavdework@gmail.com";
+        public static readonly string ClickSendApiKey = "F057035E-8019-E792-987F-6466C81E6B36";
+    }
+}
